5a153580e51aa98f1a34962354920b66480d123c
agenceefficads <yazid.b@agence-efficads.com>
2025-11-03 18:58:33 +0100

    Add files via upload

---
+++ config-public.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    N8N_WEBHOOK_TAKEOVER: 'https://demo.example.com/webhook/takeover',
+    N8N_WEBHOOK_SEND: 'https://demo.example.com/webhook/send',
+    N8N_WEBHOOK_GET_MESSAGES: 'https://demo.example.com/webhook/messages',
+    
+    API_KEY: 'demo_public_key',
+    AIRTABLE_TOKEN: 'patDEMO',
+    AIRTABLE_BASE_ID: 'appDEMO',
+    
+    AGENT_NAME: 'Demo',
+    REFRESH_INTERVAL: 120000
+};
